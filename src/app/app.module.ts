1f6a1bdc75153f50bfddea48bee51bfbde0cea8e
Rodrigo Gonzales <rgonzalesm1997@gmail.com>
2021-07-27 10:29:09 -0500

    base href

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { TableModule } from 'primeng/table';
 import { AuthModule } from './auth/auth.module';
 import { ComponentsModule } from './components/components.module';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,7 @@ import { ComponentsModule } from './components/components.module';
     AuthModule,
     ComponentsModule
   ],
-  providers: [],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/deltaIntranet/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
